d92243b39ba7850c14c83cb871e0118e4d20fb84
Anisa <anisahamohamed@gmail.com>
2020-04-20 13:09:55 -0400

    add person.js and utils to practice import and and export of functions

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-console.log("app.js is running");
+import { square, add } from "./utils.js";
+import { isAdult, canDrink } from "./person.js"
+console.log("app.js is running!");
+console.log(square(2));
+console.log(add(4, 5));
+console.log(isAdult(22));
+console.log(canDrink(20));
+

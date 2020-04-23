e07b68cc06519816f0c02c0b40d774be4ac336be
Anisa <anisahamohamed@gmail.com>
2020-04-22 21:46:27 -0400

    connected babel loader and webpack

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import validator from "validator";
+console.log(validator.isEmail("test@gmail.com"));
+import React from "react";
+import ReactDOM from "react-dom";
+
+const template = <p>testing 123</p> 
+
+ReactDOM.render(template, document.getElementById('app'))
